2528e9c412e050e032e082d4e656b6acb1ff4604
agustin valencia <aguvalenciag@gmail.com>
2025-10-30 18:39:22 +0100

    planning docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::path::Path;
+
+use anyhow::Result;
+use comrak::nodes::{AstNode, NodeValue};
+use comrak::{Arena, Options, parse_document};
+
+fn load_doc(s: String) -> Result<String> {
+    let path = Path::new(&s);
+    let input = fs::read_to_string(path)?;
+    Ok(input)
+}
+
+fn iterate_nodes<'a>(node: &'a AstNode<'a>, depth: usize) {
+    let indent = "  ".repeat(depth);
+    match &node.data.borrow().value {
+        NodeValue::Text(t) => println!("{}Text: {}", indent, t),
+        NodeValue::Heading(h) => println!("{}Heading level: {}", indent, h.level),
+        NodeValue::Paragraph => println!("{}Paragraph", indent),
+        NodeValue::Document => println!("{}Document", indent),
+        NodeValue::CodeBlock(c) => println!("{}", format!("{}CodeBlock \n{:?}", indent, c)),
+        _ => println!(
+            "{}",
+            format!("{}Other node: {:?}", indent, node.data.borrow().value)
+        ),
+    }
+
+    for child in node.children() {
+        iterate_nodes(child, depth + 1);
+    }
+}
+
+fn main() -> Result<()> {
+    let arena = Arena::new();
+    let options = Options::default();
+
+    let file = String::from("./tmp/test.md");
+    let md = load_doc(file)?;
+
+    let root = parse_document(&arena, &md, &options);
+    iterate_nodes(root, 0);
+
+    Ok(())
 }
