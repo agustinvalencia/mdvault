dd82da2bff4a0a2655f4c8ba00a9cc2ce3addccd
agustin valencia <aguvalenciag@gmail.com>
2026-02-24 14:23:00 +0100

    chore: update homebrew formula to 0.4.0

--- packaging/homebrew/mdvault.rb
+++ packaging/homebrew/mdvault.rb
@@ -1,28 +1,28 @@
 class Mdvault < Formula
   desc "CLI tool for managing markdown vaults with structured notes and validation"
   homepage "https://github.com/agustinvalencia/mdvault"
-  version "0.3.6"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.6/mdv-0.3.6-x86_64-apple-darwin.tar.gz"
-      sha256 "46070cf83af742df1a1416f770a4575b8ff42c900cf6c5847b20d53cbd9b6d03"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.4.0/mdv-0.4.0-x86_64-apple-darwin.tar.gz"
+      sha256 "9225ae869162782602fc2f41a42aea6fb45a891f75b8549cee2c7aeab66578d7"
     end
     on_arm do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.6/mdv-0.3.6-aarch64-apple-darwin.tar.gz"
-      sha256 "458eeec771733383cafc4126e1c34f267d6771eae03aa4153638754bd13b1059"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.4.0/mdv-0.4.0-aarch64-apple-darwin.tar.gz"
+      sha256 "94691fc9e54faa73399c45cecbec1b9d6942d778ed3225960646730c4e96c1e4"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.6/mdv-0.3.6-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "22bede0db41037ce944905b89cec4e3f85b4534bded8993e3c9b484b893b9604"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.4.0/mdv-0.4.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0f19554027c504062987bdb2401eca0c16292a073e766f403d362c7726c2e3ea"
     end
     on_arm do
-      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.3.6/mdv-0.3.6-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "e671a446ea1827956898562abc1bf736dbdad0dc0312f60190dd6c4d896ab8c0"
+      url "https://github.com/agustinvalencia/mdvault/releases/download/v0.4.0/mdv-0.4.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "1d5ee6775f5fb14ca155acad7a05019550e5e8e4a9aa3b656b6f930d3f2a66db"
     end
   end
 
